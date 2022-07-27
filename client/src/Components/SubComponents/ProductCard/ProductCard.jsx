115306fcad23fc7b2ce6fab21bcb1b1ddc51c57d
WiaanDuvenhage-200307 <200307@virtualwindow.co.za>
2022-07-28 00:48:27 +0200

    Completed Product Page header

---
+++ client/src/Components/SubComponents/ProductCard/ProductCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './ProductCard.module.scss';
+
+export default function ProductCard() {
+  return (
+    <div className={style.container}>
+        <p>This is a product card</p>
+    </div>
+  )
+}
--- client/src/Pages/ProductPage.jsx
+++ client/src/Pages/ProductPage.jsx
@@ -1,6 +1,8 @@
 import React, {useEffect} from 'react';
+import ProductCard from '../Components/SubComponents/ProductCard/ProductCard';
 import Nav from '../Components/UI/Nav/Nav';
 import style from './ProductPage.module.scss';
+import headerImg from '../Assets/img/guitar.png';
 
 export default function ProductPage() {
 
@@ -11,7 +13,17 @@ export default function ProductPage() {
   return (
     <div className={style.container}>
         <Nav/>
+            <header>
+                <div className={style.overlay}>
+
+                    <div className={style.headerText}>
+                        <h2>Our Products</h2>
+                        <h4>Browse all our products for all of your musical needs. We have everything from guitars to audio headphones</h4>
+                    </div>
+                </div>
+            </header>
         <h3>This is the product page</h3>
+        <ProductCard/>
     </div>
   )
 }

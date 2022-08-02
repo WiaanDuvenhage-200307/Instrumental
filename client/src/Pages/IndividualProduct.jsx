b23cb2c49459581a83b8fc23ae7c7476c2af792d
WiaanDuvenhage-200307 <200307@virtualwindow.co.za>
2022-08-02 07:55:32 +0200

    Completed Ind. Product Component

--- client/src/Pages/IndividualProduct.jsx
+++ client/src/Pages/IndividualProduct.jsx
@@ -8,12 +8,18 @@ import { useNavigate } from 'react-router-dom';
 import Button from '../Components/UI/Button/Button';
 import minus from '../Assets/icons/minus.svg';
 import plus from '../Assets/icons/plus.svg';
+import ColorPicker from '../Components/SubComponents/ColorPicker/ColorPicker';
+import Pill from '../Components/UI/Pill/Pill';
 
 export default function IndividualProduct() {
 
     const navigate = useNavigate();
 
     const [qty, setQty] = useState(1);
+
+    const addToCart = () => {
+        alert("Gibson ES-335 has been added to cart!");
+    }
 
   return (
     <div className={style.container}>
@@ -35,17 +41,38 @@ export default function IndividualProduct() {
                 </div>
                 <br />
                 <p className={style.lowStock}>Only 2 Left!</p>
-                <Button text="Add to Cart" type="addToCart"/>
+                <hr />
+                <p className={style.pBlue}>Quantity</p>
                 <div className={style.flex}>
                     {qty == 1 ? <img className={style.disabled} src={minus} width={20}/> : <img src={minus} width={20} onClick={() => setQty(qty - 1)}/>}
                     <h4 className={style.qty}>{qty}</h4>
                     <img src={plus} width={20} onClick={() => setQty(qty + 1)}/>
                 </div>
+                <hr />
+                <p className={style.pBlue}>Select a color</p>
+                <div className={style.flex}>
+                    <ColorPicker/>
+                    <ColorPicker/>
+                    <ColorPicker/>
+                </div>
+                <hr />
+                <p className={style.pBlue}>Select Neck Height</p>
+                <div className={style.flexPill}>
+                    <Pill text="545mm"/>
+                    <Pill text="545mm"/>
+                    <Pill text="545mm"/>
+                </div>
+                <hr />
+                <Button text="Add to Cart" type="addToCart" onClick={addToCart}/>
             </div>
 
             <div className={style.right}>
                 <h3>Gibson ES-335</h3>
                 <h4>Electric Guitar</h4>
+                <div className={style.flex}>
+                    <h3 className={style.oldPrice}>R12,600</h3>
+                    <h3 className={style.totalPrice}>R8,000</h3>
+                </div>
                 <br />
                 <p className={style.aboutProductHeading}>About The Product</p>
                 <br />
@@ -54,6 +81,27 @@ export default function IndividualProduct() {
                 The pearloid dot inlay rosewood fingerboard on a hand-rolled Rounded 'C' mahogany neck remind players where it all started. Gibson's Calibrated T-Type humbucking pickups are paired with our hand-wired control assembly.
                 The result is that versatile Gibson ES tone that players have craved for over 60 years. Tuning stability and precise intonation are provided by the Vintage Deluxe tuners with Keystone buttons,
                  paired with light weight Aluminum ABR-1 bridge and Stop Bar tailpiece, anchored by steel thumb-wheels and tailpiece studs.</p>
+                 <br />
+                 <br />
+                 <div className={style.flexDesc}>
+                    <div className={style.column}>
+                        <h3>335</h3>
+                        <h4 className={style.pBlue}>Body Shape</h4>
+                    </div>
+                    <div className={style.column}>
+                        <h3>Mahogany</h3>
+                        <h4 className={style.pBlue}>Neck Material</h4>
+                    </div>
+                    <div className={style.column}>
+                        <h3>22</h3>
+                        <h4 className={style.pBlue}>Frets</h4>
+                    </div>
+                    <div className={style.column}>
+                        <h3>Gold</h3>
+                        <h4 className={style.pBlue}>Strings</h4>
+                    </div>
+
+                 </div>
             </div>
 
         </div>

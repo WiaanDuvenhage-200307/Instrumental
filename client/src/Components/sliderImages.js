c6f501e75ece06f5605f2bdb445be33d375c5101
WiaanDuvenhage-200307 <200307@virtualwindow.co.za>
2022-08-08 17:18:18 +0200

    Updated Slider Images

--- client/src/Components/sliderImages.js
+++ client/src/Components/sliderImages.js
@@ -6,14 +6,14 @@ export const sliderImages = [{
 },
 {
     id: 1,
-    url: "https://images.unsplash.com/photo-1564186763535-ebb21ef5277f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    url: "https://images.unsplash.com/photo-1622216209256-56db29568e38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2022&q=80",
     text: "Gibson ES-335",
     subText: "The best guitar on and off the stage"
 },
 {
     id: 2,
-    url: "https://images.unsplash.com/photo-1446057032654-9d8885db76c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80",
-    text: "Leno Guitar Classical 212",
-    subText: "Nothing beats the classic!"
+    url: "https://images.unsplash.com/photo-1647099793531-9d95e9b3270b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
+    text: "Fender",
+    subText: "For Guitarists. By Guitarists"
 }
 ]
